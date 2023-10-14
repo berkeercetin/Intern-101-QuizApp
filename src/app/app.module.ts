0a5ff8a39ffce36fc737006055d1d6dc1419ee60
Berke Erçetin <82932880+berkeercetin@users.noreply.github.com>
2023-10-14 10:18:12 +0300

    Tools installed
    Angular Fire, Bootstrap, Font Awasome.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideAnalytics(() => getAnalytics()), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, ScreenTrackingService,UserTrackingService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'quizapp-dee87',
+    appId: '1:736881505422:web:58c0d81e4dda12045545df',
+    storageBucket: 'quizapp-dee87.appspot.com',
+    apiKey: 'AIzaSyBa3liqhaCCexEPt_oeC-TvLUHdYJbzx2Q',
+    authDomain: 'quizapp-dee87.firebaseapp.com',
+    messagingSenderId: '736881505422',
+    measurementId: 'G-CD4HFC0NHJ',
+  },
   production: false
 };
 
