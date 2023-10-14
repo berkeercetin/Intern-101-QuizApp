d6de6d3d585e7a2f0d2aea32e2b8b51294e1fab8
Berke Erçetin <82932880+berkeercetin@users.noreply.github.com>
2023-10-14 14:19:01 +0300

    commit

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -4,22 +4,30 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthPage } from './auth.page';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: AuthPage
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'register',
-    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
-  },
-  {
-    path: 'forgot-password',
-    loadChildren: () => import('./pages/forgot-password/forgot-password.module').then( m => m.ForgotPasswordPageModule)
-  }
+   {
+     path: '',
+     component: AuthPage,
+     children:[
+      {
+        path: 'login',
+        loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+      },
+      {
+        path: 'register',
+        loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
+      },
+      {
+        path: 'forgot-password',
+        loadChildren: () => import('./pages/forgot-password/forgot-password.module').then( m => m.ForgotPasswordPageModule)
+      },
+      {
+        path: '',
+        loadChildren: () => import('./pages/landing/landing.module').then( m => m.LandingPageModule)
+      }
+
+     ]
+   },
+ 
 ];
 
 @NgModule({
